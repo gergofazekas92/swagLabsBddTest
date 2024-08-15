ef8dc1f6e3b5fe4ee87f567b1b978e0a60dba89d
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-08-15 11:14:48 +0200

    createLoginPage

--- swagLabsBdd/src/main/java/org/example/pages/LoginPage.java
+++ swagLabsBdd/src/main/java/org/example/pages/LoginPage.java
@@ -1,10 +1,13 @@
 package org.example.pages;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage{
+    private final Dotenv dotenv = Dotenv.load();
     @FindBy(id = "user-name")
     private WebElement usernameField;
     @FindBy(id = "password")
@@ -18,6 +21,53 @@ public class LoginPage extends BasePage{
 
     public LoginPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void login(String username, String password) {
+        driver.get(dotenv.get("BASE_URL"));
+        System.out.println("Navigated to login page");
+        fillUsername(username);
+        fillPassword(password);
+        submitCredentials();
+    }
+
+    public boolean checkIfLoginIsSuccessful() {
+        wait.until(ExpectedConditions.visibilityOf(menuButton));
+        return menuButton.isDisplayed();
+    }
+
+    public boolean checkIfLoginIsUnsuccessful() {
+        wait.until(ExpectedConditions.visibilityOf(loginErrorMessage));
+        return checkIfErrorMessageIsShown();
+    }
+
+    public boolean checkIfLogoutIsSuccessful() {
+        return checkIfLoginPageIsShown();
+    }
+
+    private boolean checkIfLoginPageIsShown() {
+        wait.until(ExpectedConditions.visibilityOf(loginButton));
+        return loginButton.isDisplayed();
+    }
+
+    private boolean checkIfErrorMessageIsShown() {
+        wait.until(ExpectedConditions.visibilityOf(loginErrorMessage));
+        return loginErrorMessage.isDisplayed();
+    }
+
+    private void fillUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOf(usernameField));
+        usernameField.sendKeys(username);
+    }
+
+    private void fillPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordField));
+        passwordField.sendKeys(password);
+    }
+
+    private void submitCredentials() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton));
+        loginButton.click();
     }
 
 }
