920104f91c9c4596f267640a3955e798525ce5c3
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-08-15 09:53:36 +0200

    createEnvForLogin

--- swagLabsBdd/src/main/java/org/example/pages/LoginPage.java
+++ swagLabsBdd/src/main/java/org/example/pages/LoginPage.java
@@ -1,4 +1,23 @@
 package org.example.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+    @FindBy(id = "password")
+    private WebElement passwordField;
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement menuButton;
+    @FindBy(xpath = "//div[@class = 'error-message-container error']")
+    private WebElement loginErrorMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
 }
